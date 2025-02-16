536a80b36cb45857153cc3575e76efb273fb6119
felipeloche <felipe.loche@icloud.com>
2025-02-16 04:02:40 -0300

    feat: versão final do projeto com comentários e docker

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,15 @@
+//Página Inicial
 import React from "react"
 import logo from '../assets/Logo-Vertical.png'
 
 function Home(){
     return(
+        //Container principal
         <div className="h-screen flex flex-col justify-between">
             <main className="p-8">
                 <section className="max-w-2xl mx-auto mt-4">
                     <h1 className="text-2x1 font-bold text-gray-900 mb-4">Quem somos</h1>
+                    {/* Textos informativos com espaçamento */}
                     <div className="space-y-4 text-gray-600">
                         <p>Os Anfitriões de Aluguel são uma rede colaborativa de anfitriões profissionais dedicados a proporcionar segurança, tranquilidade e rendimentos espetaculares para proprietários de imóveis que querem oferecer suas casas e apartamentos em plataformas digitais como AirBnB e Booking.</p>
                         <p>Se você busca um local para se hospedar, aqui é também o seu lugar. Criamos experiências inesquecíveis, oferecendo praticidade, conforto e segurança, em ambientes limpos, funcionais e agradáveis.</p>
@@ -14,12 +17,15 @@ function Home(){
                     </div>
                 </section>
             </main>
+            {/* Rodapé */}
             <footer className="bg-gray-900 text-white py-4">
                 <div className="container mx-auto flex justify-center items-center gap-8">
                     <div>
+                        {/* Logo do Rodapé */}
                         <img src={logo} alt="Anfitriões de Aluguel" className="h-25"/>
                     </div>
                     <div className="text-center">
+                        {/* Infos de Contato */}
                         <p className="mb-1">contato@anfitrioesdealuguel.com.br</p>
                         <p>© 2025 Anfitriões de Aluguel. All rights reserved</p>
                     </div>
